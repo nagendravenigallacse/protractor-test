70c8676426ba29e6e05b2ca45188484f0da8e258
nagendravenigallacse <nagendravenigallacse@gmail.com>
2016-11-30 15:47:57 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -5,13 +5,10 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
 
     baseUrl: '	http://183.82.2.170:4444/wd/hub',
-    chromeDriver: './lib/chromedriver'
-    chromeOnly: true,
+
     specs: ['todo-spec.js'],
     
-    onPrepare: function(){
-browser.driver.manage().window().maximize();
-    },
+   
     
     onPrepare: function() {
         jasmine.getEnv().addReporter(
